12663fa7884e9dfab0fe66b42cc42c17d92b8ad8
Verónica Montaña <veronicamontana@gmail.com>
2023-05-24 12:56:39 -0300

    create models home

--- apps/home/models.py
+++ apps/home/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+# Creacion de los modelos
+class Usuario(models.Model):
+    apellido = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100)
+    dni = models.CharField(max_length=8)
+    nacionalidad = models.CharField(max_length=30)
+    fecha_de_nacimiento = models.DateField()
+    email = models.EmailField()
+    fecha_de_alta = models.DateField()
+    domicilio = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.apellido
+    
+class Artista(models.Model):
+    apellido = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100)
+    nacionalidad = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.apellido
+    
+
+class Album(models.Model):
+    nombre = models.CharField(max_length=100)
+    fecha_de_lanzamiento = models.DateField()
+    estilo = models.DateField(max_length=50)
+    
+    def __str__(self):
+        return self.nombre
+    
+    
