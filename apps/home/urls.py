bc6b607dd78f26f557944fc082421348e67927c8
Verónica Montaña <veronicamontana@gmail.com>
2023-05-24 20:04:44 -0300

    commit crear usuario

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name='home'),
+    path("crear_usuario/", views.crear_usuario, name="crear_usuario"),
+    #path("crear_artista", views.crear_artista, name="crear_artista"),
+    #path("crear_album", views.crear_album, name="crear_album"),
+    
 ]
--- apps/home/views.py
+++ apps/home/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from . import forms
 
 # Create your views here.
 def index(request):
     return render(request, "home/index.html",)
+
+def crear_usuario(request):
+        
+    form = forms.UsuarioForm()
+    context = {"form": form}
+    return render(request, "home/crear_usuario.html", context)
 
