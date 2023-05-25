2f75886dae6314947a6117db2222d06e75f58f6b
Verónica Montaña <veronicamontana@gmail.com>
2023-05-25 08:08:11 -0300

    Creacion de Menu

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from . import forms
 
 # Create your views here.
@@ -6,9 +6,34 @@ def index(request):
     return render(request, "home/index.html",)
 
 def crear_usuario(request):
-        
-    form = forms.UsuarioForm()
-    context = {"form": form}
-    return render(request, "home/crear_usuario.html", context)
+    if request.method == "POST":
+        form = forms.UsuarioForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, "home/index.html")
+    else:
+        form = forms.UsuarioForm()
+    return render(request, "home/crear_usuario.html", {"form": form})
+
+def crear_artista(request):
+    if request.method == "POST":
+        form = forms.ArtistaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, "home/index.html")
+    else:    
+        form = forms.ArtistaForm()
+    return render(request, "home/crear_artista.html", {"form": form})
+
+def crear_album(request):
+    if request.method == "POST":
+        form = forms.AlbumForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, "home/index.html")
+    else:
+        form = forms.AlbumForm()
+        context = {"form": form}
+        return render(request, "home/crear_album.html", context)
     
 
